03681a56edbb9ee73810180d0b2b499a3b315d0a
mruiz <mruiz@ERC107-08>
2017-04-10 17:59:13 -0400

    comitting

---
+++ SWPresentation/SWPresentation/Source.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+int numerical_error(int x, int y)
+{
+	if (y == 0) throw 0;
+	return x / y;
+}
+
+
+//int main()
+//{
+//	//example 1 catch by reference (BEST PRACTICE)
+//	try
+//	{
+//		numerical_error(10, 0);
+//	}
+//	catch (int& z)
+//	{
+//		cout << "Error, you can't divide by: " << z;
+//	}
+//
+//	cin.get();
+//	return 0;
+//}
+
+int main()
+{
+	//example 2 input validation
+	//NOT BEST PRACTICE
+	int input;
+	cout << "Program start..." << endl;
+	cout << endl;
+
+	try
+	{
+		cout << "Input validation example." << endl;
+		throw 1;
+		cout << "we are getting out of the try block" << endl;
+	}
+	catch (int i)
+	{
+		cout << "Error, the exception number caught is: " << i << endl;
+	}
+	cout << "Now out of the catch block" << endl;
+
+	system("PAUSE");
+	return 0;
+}
+
