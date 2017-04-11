0ec0ee54884b70eaa3e922280b051405e956d090
Administrator <Administrator@GIGI>
2017-04-10 21:11:47 -0400

    pushed

--- SWPresentation/SWPresentation/Source.cpp
+++ SWPresentation/SWPresentation/Source.cpp
@@ -9,44 +9,48 @@ int numerical_error(int x, int y)
 	return x / y;
 }
 
-
-//int main()
-//{
-//	//example 1 catch by reference (BEST PRACTICE)
-//	try
-//	{
-//		numerical_error(10, 0);
-//	}
-//	catch (int& z)
-//	{
-//		cout << "Error, you can't divide by: " << z;
-//	}
-//
-//	cin.get();
-//	return 0;
-//}
+//EXAMPLE 1
 
 int main()
 {
-	//example 2 input validation
-	//NOT BEST PRACTICE
-	int input;
-	cout << "Program start..." << endl;
-	cout << endl;
-
+	//example 1 catch by reference (BEST PRACTICE)
 	try
 	{
-		cout << "Input validation example." << endl;
-		throw 1;
-		cout << "we are getting out of the try block" << endl;
+		numerical_error(10, 0);
 	}
-	catch (int i)
+	catch (int& z)
 	{
-		cout << "Error, the exception number caught is: " << i << endl;
+		cout << "Error, you can't divide by: " << z;
 	}
-	cout << "Now out of the catch block" << endl;
 
-	system("PAUSE");
+	cin.get();
 	return 0;
 }
+
+
+//EXAMPLE2
+
+//int main()
+//{
+//	//example 2 input validation
+//	//NOT BEST PRACTICE
+//	int input;
+//	cout << "Program start..." << endl;
+//	cout << endl;
+//
+//	try
+//	{
+//		cout << "Input validation example." << endl;
+//		throw 1;
+//		cout << "we are getting out of the try block" << endl;
+//	}
+//	catch (int i)
+//	{
+//		cout << "Error, the exception number caught is: " << i << endl;
+//	}
+//	cout << "Now out of the catch block" << endl;
+//
+//	system("PAUSE");
+//	return 0;
+//}
 
